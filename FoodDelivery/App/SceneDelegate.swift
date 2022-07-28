07e4b36acbc7b2a3709ddfc8aeaf926ed0faa66d
Mikhail Kostylev <godhunt.50cal@gmail.com>
2022-07-28 18:18:06 +0300

    Delete comments

--- FoodDelivery/App/SceneDelegate.swift
+++ FoodDelivery/App/SceneDelegate.swift
@@ -36,17 +36,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             return OnboardingViewController()
         }
     }
-    
-    // MARK: - Scene Lifecycle
-
-    func sceneDidDisconnect(_ scene: UIScene) {}
-
-    func sceneDidBecomeActive(_ scene: UIScene) {}
-
-    func sceneWillResignActive(_ scene: UIScene) {}
-
-    func sceneWillEnterForeground(_ scene: UIScene) {}
-
-    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
 
